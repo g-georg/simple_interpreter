c23f2254d50a39bbd9acc86e282ef6a2dda8e314
fabianqube <fabian.cristea@gmail.com>
2026-05-05 18:42:45 +0300

    added the final changes

--- asm/include/assembler.hpp
+++ asm/include/assembler.hpp
@@ -3,72 +3,47 @@
 #include "assembler_errors.hpp"
 #include "assembler_types.hpp"
 #include "command_table.hpp"
-#include "file.hpp"
-#include "spu_common.hpp"
 
-#include <array>
-#include <cstddef>
-#include <cstdio>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
 namespace assembler {
 
-inline constexpr int kUninitializedLabelValue = -1;
-
-struct Label {
-  unsigned long    hash          = 0;
-  ssize_t          address_value = kUninitializedLabelValue;
-  std::string_view name          = {};
-};
-
 class Assembler {
  public:
-  Assembler()  = default;
-  ~Assembler();
+  Assembler() = default;
 
-  Assembler(const Assembler&)            = delete;
-  Assembler& operator=(const Assembler&) = delete;
-
-  AssemblerErrorHandler ReadSourceFile    (std::string_view input_file_name);
-  AssemblerErrorHandler AssembleProgram   ();
-  AssemblerErrorHandler WriteBinaryOutput (std::string_view output_file_name);
+  AssemblerErrorHandler ReadSourceFile(std::string_view filename);
+  AssemblerErrorHandler AssembleProgram();
+  AssemblerErrorHandler WriteBinaryOutput(std::string_view filename) const;
 
  private:
-  AssemblerError RunFirstPass ();
-  AssemblerError RunFinalPass ();
+  AssemblerError RunFirstPass();
+  AssemblerError RunFinalPass();
 
-  AssemblerError ProcessLine   (size_t line_idx, AssemblyPass pass);
-  AssemblerError EmitCommand   (const Command* command, char** line_ptr);
-  AssemblerError ParseArgument (Argument* argument,     char** line_ptr);
+  AssemblerError ProcessLine(std::string_view line, AssemblyPass pass);
 
-  AssemblerError RegisterLabel (char** line_ptr);
-  int            LookupLabel   (std::string_view name, unsigned long hash) const;
+  const Command* LookupCommand(std::string_view& line) const;
+  AssemblerError ParseArgument(std::string_view& line, Argument& arg);
 
-  const Command* LookupCommand (char** line_ptr) const;
+  AssemblerError RegisterLabel(std::string_view& line);
+  int LookupLabel(std::string_view name) const;
 
-  AssemblerError DecodeRegister       (char* name, int* bytes_consumed, int* bytecode) const;
-  AssemblerError DecodeMemoryRegister (char* name, int* bytes_consumed, int* bytecode) const;
+  static std::string_view Trim(std::string_view s);
+  static std::string_view NextToken(std::string_view& line);
 
-  AssemblerError WriteFileHeader       (FILE* output_file) const;
-  AssemblerError WriteFileBody         (FILE* output_file) const;
-  AssemblerError EmitListingLine       (size_t instruction_start);
-  AssemblerError CheckForTrailingGarbage (char* line_ptr) const;
-  AssemblerError GrowBytecodeIfNeeded  ();
+ private:
+  std::vector<std::string> lines_;
+  std::string source_file_name_;
 
-  text_t      source_text_         = {};
-  std::string source_file_name_    = {};
-  size_t      current_line_number_ = 0;
+  std::vector<int> bytecode_;
+  size_t instruction_pointer_ = 0;
 
-  std::vector<int> bytecode_              = {};
-  size_t           instruction_pointer_   = 0;
-  size_t           instructions_capacity_ = 0;
+  std::unordered_map<std::string, int> labels_;
 
-  std::array<Label, SPU_MAX_LABELS_COUNT> labels_       = {};
-  size_t                                  labels_count_ = 0;
-
-  FILE* listing_file_ = nullptr;
+  size_t current_line_number_ = 0;
 };
 
 }
--- asm/source/main.cpp
+++ asm/source/main.cpp
@@ -1,37 +1,30 @@
 #include "assembler.hpp"
-#include "debug.hpp"
 
-#include <cstdio>
-
-namespace {
-constexpr char kDefaultOutputPath[] = "../spu/out.spu";
-}
+#include <iostream>
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s <source.asm>\n", argv[0]);
+    std::cerr << "Usage: " << argv[0] << " <source.asm>\n";
     return 1;
   }
 
   assembler::Assembler assembler;
 
-  assembler::AssemblerErrorHandler status = assembler.ReadSourceFile(argv[1]);
+  auto status = assembler.ReadSourceFile(argv[1]);
   if (!status.Ok()) {
-    fprintf(stderr, "Failed to read source file \"%s\"\n", argv[1]);
+    std::cerr << status.ToString() << "\n";
     return 1;
   }
 
   status = assembler.AssembleProgram();
   if (!status.Ok()) {
-    fprintf(stderr, "Assembly failed\n");
-    status.ToString();
+    std::cerr << status.ToString() << "\n";
     return 1;
   }
 
-  status = assembler.WriteBinaryOutput(kDefaultOutputPath);
+  status = assembler.WriteBinaryOutput("../spu/out.spu");
   if (!status.Ok()) {
-    fprintf(stderr, "Failed to write output to \"%s\"\n", kDefaultOutputPath);
-    status.ToString();
+    std::cerr << status.ToString() << "\n";
     return 1;
   }
 
