4007fe9a662f496efeecfe4c117c92abcef90b9d
Георгий <g_georg@MacBook-Air-Georgij.local>
2026-05-17 21:59:18 +0300

    edited

--- ast_frontend/main.cpp
+++ ast_frontend/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: " << argv[0] << " source.rap\n";
+        std::cerr << "Usage: " << argv[0] << " source.jpp\n";
         return 1;
     }
 
